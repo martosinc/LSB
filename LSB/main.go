fb67a1b4dca17e16b05c0022810c3d90adcc5586
Maxim Mazur <60327729+martosinc@users.noreply.github.com>
2021-02-06 16:42:52 +0300

    Add files via upload

---
+++ LSB/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/stories", storiesHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/main.html", "templates/header.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "main", nil)
+}
+
+func storiesHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/stories.html", "templates/header.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "stories", nil)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/about.html", "templates/header.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "about", nil)
+}
